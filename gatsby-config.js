f1262b53d9e5af3e33278f75a043fbe0e4374eb8
Luis Hocke <Luis.Hocke@gmail.com>
2021-02-06 16:56:01 +0100

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,21 @@ module.exports = {
   siteMetadata: {
     title: "pwa-audio-demo",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-offline`,
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `pwa-audio-demo`,
+        short_name: `AudioDemo`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 };
