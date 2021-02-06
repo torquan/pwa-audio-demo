8a30a26569c17218627d2580d52367c5ccbf8e41
Luis Hocke <Luis.Hocke@gmail.com>
2021-02-06 17:00:32 +0100

    audio source

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
-import React from "react"
+import React from "react";
 
-const audio = require("../audio.mp3")
+const audio = require("../audio.mp3");
 
-export default function  IndexPage() {
+export default function IndexPage() {
   return (
     <div>
-        <audio controls>
-                <source src="audio" type="audio/mpeg" />
-                    Your browser does not support the audio tag.
-        </audio>
-        <p>Music: https://www.bensound.com</p>
-
+      <audio controls>
+        <source src={audio} type="audio/mpeg" />
+        Your browser does not support the audio tag.
+      </audio>
+      <p>Music: https://www.bensound.com</p>
     </div>
-  )
+  );
 }
-
